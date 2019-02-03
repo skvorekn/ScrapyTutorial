50e87cb481cec20a4c809b98403d221fab91b298
skvorekn <skvorekn@miamioh.edu>
2019-02-03 12:02:48 -0500

    pipelines

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -5,7 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+# Scraped data -> item container -> pipeline -> SQL/Mongo database
+# activate pipeline in settings.py
 
 class QuotetutorialPipeline(object):
     def process_item(self, item, spider):
+
+        print("Pipeline :" + item['title'][0])
         return item
