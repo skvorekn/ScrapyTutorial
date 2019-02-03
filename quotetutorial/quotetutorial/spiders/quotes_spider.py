32bf747da2a0f0d16f7a287a7041a378ab3bb56a
skvorekn <skvorekn@miamioh.edu>
2019-02-03 11:42:42 -0500

    scrape quote data into dictionary

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -12,11 +12,28 @@ class QuoteSpider(scrapy.Spider):
     def parse(self, response):
         # don't return title tag (css condition)
         # css selector
-        title = response.css('title::text').extract()
+        # title = response.css('title::text').extract()
         # shown as a dictionary
         # return keyword usually, we use yield instead because it is usually used with a generator
         # that is used by Scrapy behind the scenes
-        yield {'titletext' : title}
+        # yield {'titletext' : title}
+
+        # just extract the first element
+        all_div_quotes = response.css('div.quote')
+
+        for quote in all_div_quotes:
+
+            # don't do extract because want inside the class
+            title = quote.css('span.text::text').extract()
+            author = quote.css('.author::text').extract()
+            tag = quote.css('.tag::text').extract()
+            # always yield a dictionary
+            yield {
+                'title' : title,
+                'author' : author,
+                'tag' : tag
+            }
+
 
 # notes
 # scrapy shell "websitename"
@@ -24,3 +41,5 @@ class QuoteSpider(scrapy.Spider):
 # xpath in terminal
 # combine css selector and xpath selector to give condition to extract data
 # response.css("li.next a").xpath("@href").extract()
+
+
